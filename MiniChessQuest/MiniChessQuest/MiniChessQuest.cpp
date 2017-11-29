b0a180b1cd4428e9bd79bddc6a97073424425c8c
Alberto Alvarez <alvarezalberto1992@gmail.com>
2017-11-29 12:11:05 +0100

    testing

--- MiniChessQuest/MiniChessQuest/MiniChessQuest.cpp
+++ MiniChessQuest/MiniChessQuest/MiniChessQuest.cpp
@@ -4,36 +4,82 @@
 #include "code\header\Game.hpp"
 #include <iostream>
 #include <bitset>
+#include <cmath>
+
+struct Vector3D
+{
+	float x, y, z;
+};
+
+Vector3D & GetNormalised_Vector(const Vector3D & u, const Vector3D & v)
+{
+	Vector3D result;
+	result.x = v.x - u.x;
+	result.y = v.y - u.y;
+	result.z = v.z - u.z;
+
+	std::cout << "X = " << result.x << ", Y = " << result.y << ", Z = " << result.z << std::endl;
+
+	float length = 1/sqrt(result.x * result.x + result.y * result.y + result.z * result.z);
+
+	result.x *= length;
+	result.y *= length;
+	result.z *= length;
+
+	//result.x = u.y * v.z - v.y * u.z;
+	//result.y = u.z * v.x - v.z * u.x;
+	//result.z = u.x * v.y - v.x * u.y;
+
+
+	return result;
+}
+
 int main()
 {
+	Vector3D u;
+	u.x = 3;
+	u.y = 5;
+	u.z = 7;
 
-	unsigned int main_number = 127;
-	unsigned int carry = main_number & 1; //1 is 00000001 --> by doing the number & 1, we know if the number has a 0 
-	unsigned int result = main_number ^ 1; //number XOR 1 --> will change the last bit to 1 if 0. adding 1 to the number
-	//in case the number has a 1 in the last bit then it will become 0 (substracting one)
+	Vector3D v;
+	v.x = 9;
+	v.y = 2;
+	v.z = 15;
 
-	std::cout << "carry = " << carry << " -- Result = " << result << std::endl;
-	std::cout << "binary representation = " << std::bitset<8>(main_number) <<
-		" -- binary carry = " << std::bitset<8>(carry) << " -- binary result = " << std::bitset<8>(result) << " -- binary addition = "
-		<< std::bitset<8>(1) << std::endl;
+	Vector3D r = GetNormalised_Vector(u, v);
 
-	while (carry != 0)
-	{
-		unsigned int shiftedcarry = carry << 1;
+	std::cout << "X = " << u.x << ", Y = " << u.y << ", Z = " << u.z << std::endl;
+	std::cout << "X = " << v.x << ", Y = " << v.y << ", Z = " << v.z << std::endl;
+	std::cout << "X = " << r.x << ", Y = " << r.y << ", Z = " << r.z << std::endl;
 
-		std::cout << "Shifted carry = " << shiftedcarry << " -- binary = " << std::bitset<8>(shiftedcarry) << std::endl;
 
-		carry = result & shiftedcarry;
+	//unsigned int main_number = 127;
+	//unsigned int carry = main_number & 1; //1 is 00000001 --> by doing the number & 1, we know if the number has a 0 
+	//unsigned int result = main_number ^ 1; //number XOR 1 --> will change the last bit to 1 if 0. adding 1 to the number
+	////in case the number has a 1 in the last bit then it will become 0 (substracting one)
 
-		std::cout << std::bitset<8>(result) << " ^" << std::endl;
-		std::cout << std::bitset<8>(shiftedcarry) << std::endl;
-		std::cout << "--------" << std::endl;
-		result ^= shiftedcarry;
-		std::cout << std::bitset<8>(result) << std::endl;
+	//std::cout << "carry = " << carry << " -- Result = " << result << std::endl;
+	//std::cout << "binary representation = " << std::bitset<8>(main_number) <<
+	//	" -- binary carry = " << std::bitset<8>(carry) << " -- binary result = " << std::bitset<8>(result) << " -- binary addition = "
+	//	<< std::bitset<8>(1) << std::endl;
 
-	}
+	//while (carry != 0)
+	//{
+	//	unsigned int new_carry = carry << 1;
 
-	std::cout << "Result = " << result << std::endl;
+	//	std::cout << "Shifted carry = " << new_carry << " -- binary = " << std::bitset<8>(new_carry) << std::endl;
+
+	//	carry = result & new_carry;
+
+	//	std::cout << std::bitset<8>(result) << " ^" << std::endl;
+	//	std::cout << std::bitset<8>(new_carry) << std::endl;
+	//	std::cout << "--------" << std::endl;
+	//	result = result ^ new_carry;
+	//	std::cout << std::bitset<8>(result) << std::endl;
+
+	//}
+
+	//std::cout << "Result = " << result << std::endl;
 
 	/*Game * game = new Game(10, 10);
 	
